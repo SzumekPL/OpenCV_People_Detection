1631be1a6cd73a7b3469097a870f5137cbe0dffd
SzumekPL <mateusz.szumilas@wp.pl>
2019-04-16 19:33:06 +0200

    only cascade

--- video.py
+++ video.py
@@ -3,13 +3,17 @@ import time
 
 person_cascade = cv2.CascadeClassifier('.//data//haarcascade_fullbody.xml')
 cap = cv2.VideoCapture('.//test.mp4')
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
 while True:
     r, frame = cap.read()
     if r:
         start_time = time.time()
         frame = cv2.resize(frame,(640,360)) # Downscale to improve frame rate
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY) # Haar-cascade classifier needs a grayscale image
-        rects = person_cascade.detectMultiScale(gray_frame)
+        rects = person_cascade.detectMultiScale(image = gray_frame, scaleFactor = 1.05, minNeighbors = 3)
         
         end_time = time.time()
         print("Elapsed Time:",end_time-start_time)
